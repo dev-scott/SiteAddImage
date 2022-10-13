852d5ede565942f6af1f78f0cb83a3e43cc7eb95
dev-scott <95773992+dev-scott@users.noreply.github.com>
2022-10-13 04:55:46 +0200

    start application

---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import pictureReducer from "../feature/pictures.slice"
+
+export default configureStore({
+
+    reducer:{
+    
+        pictures : pictureReducer,
+    
+    }
+
+});
